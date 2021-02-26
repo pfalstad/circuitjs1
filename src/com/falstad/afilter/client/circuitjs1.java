2d2f1a27b86526dc5442ca4f6f7bd517e917056e
pf <paul@falstad.com>
2021-02-26 11:29:58 -0800

    0.97

--- src/com/falstad/afilter/client/circuitjs1.java
+++ src/com/falstad/afilter/client/circuitjs1.java
@@ -57,7 +57,7 @@ import com.google.gwt.user.client.Window.ClosingEvent;
 
 public class circuitjs1 implements EntryPoint {
 	
-	public static final String versionString="0.96js";
+	public static final String versionString="0.97js";
 
 	static CirSim mysim;
 	
