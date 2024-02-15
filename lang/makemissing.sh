51d6a29b3148f664577db1b146ddd2916a04fca9
Taisuke Fukuno <fukuno@jig.jp>
2024-02-16 07:13:02 +0900

    add ja for makemissing.sh

--- lang/makemissing.sh
+++ lang/makemissing.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 echo run getstrings first
-for i in da de es fr it nb pl pt ru zh zh-tw
+for i in da de es fr it nb pl pt ru zh zh-tw ja
 do
   python3 findmissing.py ../src/com/lushprojects/circuitjs1/public/locale_$i.txt > missing_$i.txt
 done
