a38971617827193f6e03296cbde13c3fba7d2b08
Paul Falstad <paul@falstad.com>
2026-04-19 15:07:32 -0700

    remove node numbers

--- src/com/lushprojects/circuitjs1/client/UIManager.java
+++ src/com/lushprojects/circuitjs1/client/UIManager.java
@@ -799,6 +799,7 @@ public class UIManager {
 		    info[0] = Locale.LS(info[0]);
 		    if (info[1] != null)
 			info[1] = Locale.LS(info[1]);
+/*
 		    // show node numbers for debugging
 		    int ni;
 		    for (ni = 0; info[ni] != null && ni < info.length-1; ni++)
@@ -809,6 +810,7 @@ public class UIManager {
 			    nodeStr += " " + mouse.getMouseElm().getNode(nn).index;
 			info[ni] = nodeStr;
 		    } catch (Exception e) {}
+*/
 		} else
 		    info[0] = "V = " +
 			CircuitElm.getUnitText(mouse.getMouseElm().getPostVoltage(mouse.mousePost), "V");
